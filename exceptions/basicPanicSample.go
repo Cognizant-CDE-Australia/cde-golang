a904c3be01ea458ae426095dbbe28b76c04e3f1e
Hari Dhanakoti <haridhanakoti@gmail.com>
2019-11-05 19:16:31 +1100

    Updated sample

--- exceptions/basicPanicSample.go
+++ exceptions/basicPanicSample.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
+	//Create panic with a message
+	// panic("Error / exception message")
+
+	_, err := os.Create("c:/tmp/file")
+	if err != nil {
+		panic(err)
+	}
+
 	var m map[string]int
 	if m == nil {
 		println("m is nil")
